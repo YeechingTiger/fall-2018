81201cb0324fb8cd7ed888701fe3682613eaf51f
dlemas <djlemas@ufl.edu>
2018-08-27 21:21:18 -0400

    tiny commit

--- docker-images.R
+++ docker-images.R
@@ -1,6 +1,7 @@
-# Bioconductor Docker Images
-# https://www.bioconductor.org/help/docker/
+# Docker Images (https://www.bioconductor.org/help/docker/)
 
 # bioconductor/devel_core2
 docker pull bioconductor/devel_core2
 
+# rocker/rstudio
+docker pull rocker/rstudio
