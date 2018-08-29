e9b3da6e49775dd6b4c5597db82c53fcc340f544
dlemas <djlemas@ufl.edu>
2018-08-28 20:28:57 -0400

    rstudio changes

--- docker-images.R
+++ docker-images.R
@@ -3,8 +3,25 @@
 # bioconductor/devel_core2
 docker pull bioconductor/devel_core2
 
-# rocker/rstudio
+# 1) open docker teminal
+
+# 2) pull rocker/rstudio
 docker pull rocker/rstudio
 
-# new small change
+# 3) navigate to location of data thorugh CLI
+# $ cd Documents/MyMetabolomics
+
+# 4) run docker and mount "working directory" to directory "/home/data" in docker containter
+# $ docker run -it -v "$(pwd)":/home/data bioconductor/release_metabolomics2 /bin/bash
+
+# boot into rstudio
+docker run -it -v "$(pwd)":/home/rstudio/data --rm -p 8787:8787 -e PASSWORD=gator rocker/rstudio
+
+# open browswer and type into search bar
+http://192.168.99.100:8787
+
+
+
+
+
 
