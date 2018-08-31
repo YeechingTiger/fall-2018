5d66c07eb2055e5b738aec6f6c5fc13ee436120e
dlemas <djlemas@ufl.edu>
2018-08-30 21:13:01 -0400

    rstudio with share folder

--- rstudio-docker.R
+++ rstudio-docker.R
@@ -15,7 +15,10 @@ docker pull rocker/rstudio
 # $ docker run -it -v "$(pwd)":/home/data bioconductor/release_metabolomics2 /bin/bash
 
 # boot into rstudio
-docker run -it -v /$(pwd):/home/ --rm -p 8787:8787 -e PASSWORD=gator rocker/rstudio
+docker run --rm -d -p 8787:8787 -e PASSWORD=gator rocker/rstudio
+
+# boot into rstudio with share folder
+docker run -v /c/Users/djlemas/Documents/MyMetabolomics:/home/rstudio/data --rm -p 8787:8787 -e PASSWORD=gator rocker/rstudio
 
 # open browswer and type into search bar
 http://192.168.99.100:8787
